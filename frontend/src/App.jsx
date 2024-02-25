f9d72bdcaeeab3fc8a6307922d5d37afe5c84f62
alitarika <alitarikalacam@gmail.com>
2024-02-26 01:26:05 +0300

    logo svg - basic color palette orange dark light

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,10 @@
 import { useState } from "react";
+import Layout from "./pages/Layout";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-  const handleDecrement = () => {
-    setCount(count - 1);
-  };
   return (
     <>
-      <p className="px-14 py-2">{count}</p>
-      <button className="px-4 py-1 bg-green-700 mx-2" onClick={handleIncrement}>
-        +
-      </button>
-      <button className="px-4 py-1 bg-red-700 mx-2" onClick={handleDecrement}>
-        -
-      </button>
+      <Layout />
     </>
   );
 }
---
+++ frontend/src/pages/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <div className="w-full h-36 bg-dark "></div>
+      <div className="w-full h-12 bg-primary"></div>
+    </>
+  );
+};
+
+export default Layout;
